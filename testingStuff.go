9e82463c7951e4d1d478cb6246e71b7f5115184e
Dan Ortega <ortega.dan2010@gmail.com>
2021-04-14 09:47:40 -0500

    Stringer interface (String() method) exercise (toString() equivalent on Java)

--- testingStuff.go
+++ testingStuff.go
@@ -2,36 +2,20 @@ package main
 
 import "fmt"
 
-// fibonacci is a function that returns
-// a function that returns an int.
-func fibonacci() func() int {
+type IPAddr [4]byte
 
-	var actualFibo func(int) int
-
-	actualFibo = func(numba int) int {
-		switch numba {
-		case 0:
-			return 0
-		case 1:
-			return 1
-		default:
-			return actualFibo(numba-1) + actualFibo(numba-2)
-		}
-	}
-
-	currentIndex := 0
-
-	return func() int {
-		currentIndex += 1
-
-		return actualFibo(currentIndex)
-	}
-
+func (ipa IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ipa[0], ipa[1], ipa[2], ipa[3])
 }
 
 func main() {
-	f := fibonacci()
-	for i := 0; i < 12; i++ {
-		fmt.Print(" ", f())
+
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
 	}
 }

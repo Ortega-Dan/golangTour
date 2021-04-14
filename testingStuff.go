43b5d95c0daefb64f24143f8723d79b7d8a08853
Dan Ortega <ortega.dan2010@gmail.com>
2021-04-14 12:46:18 -0500

    Reader example (a real reader would need first to be initialized)

--- testingStuff.go
+++ testingStuff.go
@@ -1,41 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"math"
+	"golang.org/x/tour/reader"
 )
 
-type ErrNegativeSqrt float64
+type MyReader struct{}
 
-func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
-}
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+func (r MyReader) Read(array []byte) (int, error) {
 
-func Sqrt(x float64) (float64, error) {
+	slice := array[:cap(array)]
 
-	if x < 0 {
-		return 0, ErrNegativeSqrt(x)
+	for index := range slice {
+		slice[index] = 'A'
 	}
 
-	lastZ := 0.0
-
-	z := 200.0
-
-	for i := 0; true; i++ {
-		z -= (z*z - x) / (2 * z)
-
-		fmt.Printf("Iteration %v, with result %v\n", i, z)
-		if math.Abs(z-lastZ) < 0.000001 {
-			// fmt.Println()
-			return z, nil
-		}
-		lastZ = z
-	}
-	// fmt.Println()
-	return z, nil
+	return cap(slice), nil
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
-	fmt.Println(Sqrt(-2))
+	reader.Validate(MyReader{})
 }

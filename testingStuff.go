536abceaab3bbc7196b8c57ad3c6a587c4394176
Dan Ortega <ortega.dan2010@gmail.com>
2021-04-14 18:10:00 -0500

    funny example of channels (tick tick Boom!)

--- testingStuff.go
+++ testingStuff.go
@@ -1,42 +1,24 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/png"
-	"os"
+	"fmt"
+	"time"
 )
 
-type MyImage struct{}
-
-// ColorModel returns the Image's color model.
-func (img MyImage) ColorModel() color.Model {
-	return color.RGBAModel
-}
-
-// Bounds returns the domain for which At can return non-zero color.
-// The bounds do not necessarily contain the point (0, 0).
-func (img MyImage) Bounds() image.Rectangle {
-	return image.Rect(0, 0, 754, 333)
-}
-
-// At returns the color of the pixel at (x, y).
-// At(Bounds().Min.X, Bounds().Min.Y) returns the upper-left pixel of the grid.
-// At(Bounds().Max.X-1, Bounds().Max.Y-1) returns the lower-right one.
-func (img MyImage) At(x, y int) color.Color {
-
-	// answer := uint8((x + y) / 2)
-	// answer := uint8(x * y)
-	answer := uint8(x ^ y)
-	// answer := uint8(x % (y + 1))
-
-	return color.RGBA{25, answer, 255, 255}
-}
-
 func main() {
-	m := MyImage{}
-	// pic.ShowImage(m)
+	tick := time.Tick(1 * time.Second)
+	boom := time.After(5 * time.Second)
 
-	out, _ := os.Create("outie.png")
-	png.Encode(out, m)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
 }

7c77e3d13337acd2ae68e18b787f5c4973b2e830
Dan Ortega <ortega.dan2010@gmail.com>
2021-04-15 15:25:28 -0500

    playing with goroutines waiter

--- testingStuff.go
+++ testingStuff.go
@@ -3,97 +3,23 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
-// fakeFetcher is Fetcher that returns canned results.
-type fakeFetcher map[string]*fakeResult
-
-type fakeResult struct {
-	body string
-	urls []string
-}
-
-// fetcher is a populated fakeFetcher.
-var fetcher = fakeFetcher{
-	"https://golang.org/": &fakeResult{
-		"The Go Programming Language",
-		[]string{
-			"https://golang.org/pkg/",
-			"https://golang.org/cmd/",
-		},
-	},
-	"https://golang.org/pkg/": &fakeResult{
-		"Packages",
-		[]string{
-			"https://golang.org/",
-			"https://golang.org/cmd/",
-			"https://golang.org/pkg/fmt/",
-			"https://golang.org/pkg/os/",
-		},
-	},
-	"https://golang.org/pkg/fmt/": &fakeResult{
-		"Package fmt",
-		[]string{
-			"https://golang.org/",
-			"https://golang.org/pkg/",
-		},
-	},
-	"https://golang.org/pkg/os/": &fakeResult{
-		"Package os",
-		[]string{
-			"https://golang.org/",
-			"https://golang.org/pkg/",
-		},
-	},
-}
-
-type Fetcher interface {
-	// Fetch returns the body of URL and
-	// a slice of URLs found on that page.
-	Fetch(url string) (body string, urls []string, err error)
-}
-
-func (f fakeFetcher) Fetch(url string) (string, []string, error) {
-	if res, ok := f[url]; ok {
-		return res.body, res.urls, nil
-	}
-	return "", nil, fmt.Errorf("not found: %s", url)
-}
-
-// Crawl uses fetcher to recursively crawl
-// pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher) {
+func say(s string) {
 	defer waiter.Done()
-	// This implementation doesn't do either:
-	if depth <= 0 {
-		return
-	}
-
-	_, alreadyInThere := mappie.LoadOrStore(url, true)
-
-	if alreadyInThere {
-		return
-	}
-	body, urls, err := fetcher.Fetch(url)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("found: %s %q\n", url, body)
-
-	for _, u := range urls {
-		waiter.Add(1)
-		go Crawl(u, depth-1, fetcher)
+	for i := 0; i < 5; i++ {
+		time.Sleep(time.Millisecond)
+		fmt.Println(s)
 	}
 }
 
-var mappie sync.Map
 var waiter sync.WaitGroup
 
 func main() {
-	waiter.Add(1)
-	go Crawl("https://golang.org/", 4, fetcher)
+	waiter.Add(2)
+	go say("world")
+	say("hello")
+
 	waiter.Wait()
-	// time.Sleep(1 * time.Second)
 }

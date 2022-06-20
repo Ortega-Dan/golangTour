7a9d3116bc7bcb6144018360c9a2af6f2de34319
Dan Ortega <ortega.dan2010@gmail.com>
2022-06-20 11:15:25 -0500

    safe concurrency with mutex

---
+++ B_concurrency/3_mutex.go
@@ -0,0 +1,40 @@
+package B_concurrency
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]int
+}
+
+// Inc increments the counter for the given key.
+func (c *SafeCounter) Inc(key string) {
+	c.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	c.v[key]++
+	c.mu.Unlock()
+}
+
+// Value returns the current value of the counter for the given key.
+func (c *SafeCounter) Value(key string) int {
+	c.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	defer c.mu.Unlock()
+	return c.v[key]
+}
+
+// mutex is the basis for concurrency safe objects
+func Mutex_main() {
+	c := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c.Inc("somekey")
+	}
+
+	time.Sleep(3 * time.Millisecond)
+	fmt.Println(c.Value("somekey"))
+}
